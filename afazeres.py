dbb20fe79cb46911f8c4a335d565096c311f7bb7
Manuely <manuely.candido@aluno.senai.br>
2025-08-01 11:19:48 -0300

    Estou continuando o jogo

---
+++ afazeres.py
@@ -0,0 +1,42 @@
+lista_tarefas = []
+while True:
+    print("""
+*****************************
+*     LISTA                 *
+*           DE              *
+*              AFAZERES     *
+*****************************
+* 1 - iNSERIR TAREFA        *
+* 2 - LISTAR TAREFA         *
+* 3 - MARCAR COMO CONCLUIDO *
+* 4 - REMOVER TAREFA        *
+* 0 - SAIR                  *
+*****************************
+""")
+    lista = int(input("Escolha um número: "))
+    
+    if lista == 1:
+        print("Você escolheu a opção número 1")
+        adicionartarefa = input("Adicione a tarefa: ")
+        lista_tarefas.append(adicionartarefa)
+        print (lista_tarefas)
+
+    elif lista == 2:
+        print("Você escolheu o número 2")
+        for tarefa in lista_tarefas:
+            print(tarefa)
+        
+
+    elif lista == 3:
+        print("Você escolheu o número 3")
+
+
+    elif lista == 4:
+        print("Você escolheu o número 4")
+        item_removido = input("Qual tarefa voce deseja remover: ")
+
+
+        lista_tarefas.remove
+    elif lista == 0:
+        print("Você escolheu sair")
+        break
